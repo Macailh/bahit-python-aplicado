9e94dd4320838ce4db2981bbc961754e0fdfaa28
Macailh <salvadorenriquegb@gmail.com>
2023-09-24 19:10:05 -0600

    Add operators

---
+++ logical_operators/bitwise_operators.py
@@ -0,0 +1,18 @@
+print('----Bitwise Operators----')
+
+# Bitwise AND operator (&)
+print(5 & 3)  # In binary: 101 & 011 = 001 (in decimal: 1)
+
+# Bitwise OR operator (|)
+print(5 | 3)  # In binary: 101 | 011 = 111 (in decimal: 7)
+
+# Bitwise XOR operator (^)
+print(5 ^ 3)  # In binary: 101 ^ 011 = 110 (in decimal: 6)
+
+# Left shift (<<)
+print(5 << 1)  # In binary: 101 shifted 1 left = 1010 (in decimal: 10)
+
+# Right shift (>>)
+print(5 >> 1)  # In binary: 101 shifted 1 right = 10 (in decimal: 2)
+
+print('-------------------------')
---
+++ logical_operators/operators.py
@@ -0,0 +1,26 @@
+print('----and operator------')
+print(1 > 0 and 4 < 5)
+print(5 == 7 and 7 < 12)
+print('----------------------')
+
+print('----or operator-------')
+print(12 == 12 or 15 < 7)
+print(3 > 5 or 13 < 12)
+print('----------------------')
+
+print('----xor operator------')
+print((4 == 4) ^ (3 > 4))
+print(4 == 4)
+print(3 > 4)
+print('----------------------')
+
+print('----not operator------')
+print(not True)
+print(not False)
+print(not (2 > 1))
+print('----------------------')
+
+print('----equality operator------')
+print(5 == 5)
+print(10 == 5)
+print('----------------------')
