da83c4c8634f9201320c88ef17c418e8ca1fa0a5
Macailh <salvadorenriquegb@gmail.com>
2023-10-14 11:03:22 -0600

    Add collection and concatenation

---
+++ annex_lists_and_tuples/collection_concatenation.py
@@ -0,0 +1,13 @@
+print("==================================")
+print("Concatenation of lists")
+list_one = [1, 2, 3, 4, 5]
+list_two = [6, 7, 8, 9, 10]
+list_three = list_one + list_two
+print(list_three)
+
+print("==================================")
+print("Concatenation of tuples")
+tuple_one = (1, 2, 3, 4, 5)
+tuple_two = (6, 7, 8, 9, 10)
+tuple_three = tuple_one + tuple_two
+print(tuple_three)
