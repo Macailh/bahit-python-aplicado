df016ce84d74babd169ef99a94902f68cf0ab502
Macailh <salvadorenriquegb@gmail.com>
2023-10-14 11:03:39 -0600

    Add coversion of type

---
+++ annex_lists_and_tuples/conversion_of_types.py
@@ -0,0 +1,14 @@
+print("==================================")
+print("Example list and tuple")
+ex_tuple = (1, 2, 3, 4)
+ex_list = [1, 2, 3, 4, 4, 4]
+print(ex_tuple)
+print(ex_list)
+
+print("==================================")
+print("Conversion list to tuple")
+print(list(ex_tuple))
+
+print("==================================")
+print("Conversion tuple to list")
+print(tuple(ex_list))
