8714f67cb5728dbb72d69ec7de644ccdedf93f1a
Macailh <salvadorenriquegb@gmail.com>
2023-10-15 10:59:20 -0600

    Add max and min values

---
+++ annex_lists_and_tuples/max_and_min_values.py
@@ -0,0 +1,11 @@
+print("==================================")
+print("Max value form a list and tuple with max()")
+ex_list = [1, 2, 3, 4, 5, 6]
+ex_tuple = (7, 54, 4, 3, 23, 45)
+print(max(ex_list))
+print(max(ex_tuple))
+
+print("==================================")
+print("Min value form a list and tuple with min()")
+print(min(ex_list))
+print(min(ex_tuple))
