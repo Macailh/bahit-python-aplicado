af41e292aa41acd02bf49f615988f8bbfcafae55
Macailh <salvadorenriquegb@gmail.com>
2023-10-15 10:59:11 -0600

    Add count elements

---
+++ annex_lists_and_tuples/count_elements.py
@@ -0,0 +1,6 @@
+print("==================================")
+print("Count elements with len()")
+ex_list = [1, 2, 3, 4, 5, 6]
+ex_tuple = (7, 54, 4, 3, 23, 45)
+print(len(ex_list))
+print(len(ex_tuple))
