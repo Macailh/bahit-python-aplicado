9b048a5afd409116512468ec75de0a06f3d5672e
Macailh <salvadorenriquegb@gmail.com>
2023-10-16 07:33:14 -0600

    Add delete methods for dictionaries

---
+++ manipulation_dictionaries/delete_methods.py
@@ -0,0 +1,6 @@
+print("==================================")
+print("Empty dictionary with clear()")
+ex_dictionary = {"color": "purple", "size": "M", "price": 165.43}
+print(ex_dictionary)
+ex_dictionary.clear()
+print(ex_dictionary)
