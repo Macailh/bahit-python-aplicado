d4cd724b56a84d50bbdf742cc2fab8c05a8b61fa
Macailh <salvadorenriquegb@gmail.com>
2024-02-17 21:19:19 -0600

    Add product and square root

---
+++ native_mathematics/product_square_root.py
@@ -0,0 +1,14 @@
+from math import prod, pow, sqrt
+
+number = 9
+print("The square root of", number, "is:", sqrt(number))
+print("The product of 2 and 3 is:", prod([2, 3]))
+print("The power of 2 to 3 is:", pow(2, 3))
+
+print("The power of 2 to 3 is:", pow(2, 3))
+print()
+print("The square root of 1000 is:", sqrt(1000))
+print("The product of 10 and 100 is:", prod([10, 100]))
+print("The power of 10 to 100 is:", pow(10, 100))
+print()
+
